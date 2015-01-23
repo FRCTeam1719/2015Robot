7dc1c03895b36a99458de3080c80d77ee5a0475b
Gold Dragon <dgolddragon28@gmail.com>
2015-01-23 16:56:41 -0500

    Completed first attempt at camera control swaps

--- 2015Robot/src/org/usfirst/frc1719/commands/TransferCameraControl.java
+++ 2015Robot/src/org/usfirst/frc1719/commands/TransferCameraControl.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc1719.commands;
 
+import org.usfirst.frc1719.OI;
+import org.usfirst.frc1719.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -7,9 +10,16 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class TransferCameraControl extends Command {
 
-    public TransferCameraControl() {
+	public static final boolean OPERATOR = true;
+	public static final boolean DRIVER = false;
+	private static boolean controller = DRIVER;
+	private static boolean done = false;
+	private final boolean who;
+	
+    public TransferCameraControl(boolean par1) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	who = par1;
     }
 
     // Called just before this Command runs the first time
@@ -18,13 +28,25 @@ public class TransferCameraControl extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	// Hack -- make sure this command was called by the person currently in control.
+    	if(!done && (controller == who)) {
+    		controller = !controller;
+    		done = true;
+    	}
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        if(done) {
+        	done = false;
+        	return true;
+        } else return false;
     }
 
+    public static boolean getController() {
+    	return controller;
+    }
+    
     // Called once after isFinished returns true
     protected void end() {
     }
--- 2015Robot/src/org/usfirst/frc1719/commands/UseDrive.java
+++ 2015Robot/src/org/usfirst/frc1719/commands/UseDrive.java
@@ -60,7 +60,9 @@ public class  UseDrive extends Command {
     	//gets values from the joystick
     	double ly = Robot.oi.getJoystick1().getRawAxis(LEFT_Y);
     	double lx = Robot.oi.getJoystick1().getRawAxis(LEFT_X);
-    	double rx = Robot.oi.getJoystick1().getRawAxis(RIGHT_X);
+    	double rx = (!TransferCameraControl.getController()
+    			&& Robot.oi.getJoystick1().getRawButton(DriveServos.JOYSTICK_RIGHT_BUTTON)) ? 
+    			0.0D : Robot.oi.getJoystick1().getRawAxis(RIGHT_X);
     	
     	//creates a dead zone within tolerance in order to make it possible to stop the robot
     	if (Math.abs(ly) < TOLERANCE) ly = 0.0D;
