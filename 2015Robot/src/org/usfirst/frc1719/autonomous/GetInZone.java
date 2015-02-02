a0eb6a0b013b6c44b4a84204800f4c08a9268bc2
Gold Dragon <dgolddragon28@gmail.com>
2015-02-02 18:52:57 -0500

    Use encoder distance instead of time

--- 2015Robot/src/org/usfirst/frc1719/autonomous/GetInZone.java
+++ 2015Robot/src/org/usfirst/frc1719/autonomous/GetInZone.java
@@ -1,29 +1,25 @@
 package org.usfirst.frc1719.autonomous;
 
-import java.util.Date;
-
 import org.usfirst.frc1719.Robot;
 
 public class GetInZone implements ICommandOption {
 
 	private static final double NIL = 0.0D;
 	private static final double SPD = 0.5D;
-	private static final long TIME = 2000;
+	private static final double DISTANCE = 1.0D;
 	private boolean flag = true;
-	private Date start;
 
 	@Override
 	public void doCMD() {
 		if(flag) {
-			start = new Date();
-			flag = false;
+			Robot.sensors.resetEncoder10();
 		}
 		Robot.drive.moveCartesian(NIL, SPD, NIL);
 	}
 
 	@Override
 	public boolean done() {
-		return ((new Date()).getTime() - start.getTime()) > TIME;
+		return Robot.sensors.getEncoder10Distance() > DISTANCE;
 	}
 
 }
