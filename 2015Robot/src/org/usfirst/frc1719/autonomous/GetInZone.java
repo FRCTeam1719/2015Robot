905cb27c44a6457bc122c53d56f160f870369e1c
Gold Dragon <dgolddragon28@gmail.com>
2015-02-07 13:08:17 -0500

    Fixed bugs

--- 2015Robot/src/org/usfirst/frc1719/autonomous/GetInZone.java
+++ 2015Robot/src/org/usfirst/frc1719/autonomous/GetInZone.java
@@ -6,20 +6,21 @@ public class GetInZone implements ICommandOption {
 
 	private static final double NIL = 0.0D;
 	private static final double SPD = 0.5D;
-	private static final double DISTANCE = 1.0D;
+	private static final double DISTANCE = 1000.0D;
 	private boolean flag = true;
 
 	@Override
 	public void doCMD() {
 		if(flag) {
 			Robot.sensors.resetEncoder10();
+			flag = false;
 		}
 		Robot.drive.moveCartesian(NIL, SPD, NIL);
 	}
 
 	@Override
 	public boolean done() {
-		return Robot.sensors.getEncoder10Distance() > DISTANCE;
+		return Math.abs(Robot.sensors.getEncoder10Distance()) > DISTANCE;
 	}
 
 }
