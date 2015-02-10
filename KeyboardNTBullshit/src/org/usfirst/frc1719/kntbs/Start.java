8b341d4ff168a0c9db9664ac60f1a13da1fe7f83
Gold Dragon <dgolddragon28@gmail.com>
2015-02-09 19:29:49 -0500

    Made user actually know when the code is running

--- KeyboardNTBullshit/src/org/usfirst/frc1719/kntbs/Start.java
+++ KeyboardNTBullshit/src/org/usfirst/frc1719/kntbs/Start.java
@@ -7,12 +7,12 @@ import edu.wpi.first.wpilibj.networktables.NetworkTable;
 public class Start {
 
 	public static void main(String[] args) throws InterruptedException {
+		(new javax.swing.JFrame()).setVisible(true); //Make a blank window so the user knows this is active
 		NetworkTable.setClientMode();
 		NetworkTable.setIPAddress("roboRIO-1719.local");
 		NetworkTable table = NetworkTable.getTable("SmartDashboard");
 		table.putValue("keyflags", 0x0000000000000000L);
 		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyTracker(table));
-		Start.class.wait();
 	}
 
 }
