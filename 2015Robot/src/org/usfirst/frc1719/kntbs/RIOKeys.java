d659635c3e0a8d93f39344e3ada754577ae46421
Gold Dragon <dgolddragon28@gmail.com>
2015-02-09 19:23:38 -0500

    Added in RIO-side get-and-decode method to make things simpler. To get whether a key is pressed call RIOKeys.isKeyPressed(<hid>) where <hid> is the hid code for that key (which is identical to the ASCII representation for alphanumeric symbols, but may differ for others

---
+++ 2015Robot/src/org/usfirst/frc1719/kntbs/RIOKeys.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc1719.kntbs;
+
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
+public class RIOKeys {
+	private static NetworkTable table = NetworkTable.getTable("SmartDashboard");
+	public static boolean isKeyPressed(int hid) {
+		return (((long) table.getValue("keyflags")) & KeyConstants.getCode(hid)) != 0;
+	}
+}
